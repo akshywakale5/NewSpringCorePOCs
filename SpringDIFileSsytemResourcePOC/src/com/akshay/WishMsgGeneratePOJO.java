1537849a9c8bc21ecbcf17bad2fa079c78f7e7b3
akshay <akshywakale5@gmail.com>
2022-02-06 22:15:49 +0530

    Testing ClassPathResource and FileSystemResource

---
+++ SpringDIFileSsytemResourcePOC/src/com/akshay/WishMsgGeneratePOJO.java
@@ -0,0 +1,62 @@
+package com.akshay;
+
+import java.util.Date;
+
+public class WishMsgGeneratePOJO {
+
+	private String name;
+	private Date date;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((date == null) ? 0 : date.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WishMsgGeneratePOJO other = (WishMsgGeneratePOJO) obj;
+		if (date == null) {
+			if (other.date != null)
+				return false;
+		} else if (!date.equals(other.date))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "WishMsgGeneratePOJO [name=" + name + ", date=" + date + "]";
+	}
+	
+	public String msgGenerate() {
+		return "Hello "+name+" today is "+date;
+	}
+	
+	
+}
---
+++ SpringDIFileSsytemResourcePOC/src/com/akshay/WishMsgTestApp.java
@@ -0,0 +1,17 @@
+package com.akshay;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class WishMsgTestApp {
+
+	public static void main(String[] args) {
+		ClassPathResource fileSystemResource=new
+		ClassPathResource("ApplicationContext.xml");
+		//FileSystemResource fileSystemResource = new FileSystemResource("src/ApplicationContext.xml");
+		XmlBeanFactory beanFactory = new XmlBeanFactory(fileSystemResource);
+		WishMsgGeneratePOJO wish = beanFactory.getBean("i", WishMsgGeneratePOJO.class);
+		System.out.println("Result :" + wish.msgGenerate());
+	}
+
+}
