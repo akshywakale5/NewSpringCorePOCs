75979f38eebcd39a56386fd73cd676b5b8e8c780
akshay <akshywakale5@gmail.com>
2022-02-06 15:21:44 +0530

    new commit

---
+++ FormProjWithServlets/src/main/java/com/akshay/proj/PersonInfoCollectController.java
@@ -0,0 +1,25 @@
+package com.akshay.proj;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class PersonInfoCollectController extends HttpServlet {
+
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6096131921005137431L;
+
+	public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException {
+		
+		
+	}
+	
+public void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException {
+		
+		
+	}
+}
---
+++ FormProjWithServlets/src/main/webapp/WEB-INF/views/Welcome.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="/apply" name="apply" method="post">
+<h5>Hi Welcome please click below link to start filling up the form</h5>
+<h5><a href="/apply">click me</a></h5>
+</form>
+</body>
+</html>
